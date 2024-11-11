308245f396711b06c8797dc64b07ba37bcc29943
thsghdud13 <thsgdud25@naver.com>
2024-11-11 23:08:43 +0900

    feat: 실행 기능

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,12 @@
 package store;
 
+import camp.nextstep.edu.missionutils.Console;
+import store.config.ConvenienceStoreConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConvenienceStoreConfig convenienceStoreConfig = new ConvenienceStoreConfig();
+        convenienceStoreConfig.counterController().respond();
+        Console.close();
     }
 }
