06794f0ecc37677bb6bde18bfdaf9237f777a264
thsghdud13 <thsgdud25@naver.com>
2024-11-11 23:43:34 +0900

    feat: cleanUp 로직 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,11 +2,15 @@ package store;
 
 import camp.nextstep.edu.missionutils.Console;
 import store.config.ConvenienceStoreConfig;
+import store.repository.InMemoryInventoryRepository;
+import store.repository.InMemoryPromotionRepository;
 
 public class Application {
     public static void main(String[] args) {
         ConvenienceStoreConfig convenienceStoreConfig = new ConvenienceStoreConfig();
         convenienceStoreConfig.counterController().respond();
         Console.close();
+        InMemoryPromotionRepository.getInstance().clear();
+        InMemoryInventoryRepository.getInstance().clear();
     }
 }
